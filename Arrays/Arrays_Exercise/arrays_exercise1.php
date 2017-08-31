04a9ac5a2190b29f45157997bc978b22b0b02997
franisrael <franisrael@gmail.com>
2017-08-31 16:05:23 +0100

    check array exercise 1

--- Arrays/Arrays_Exercise/arrays_exercise1.php
+++ Arrays/Arrays_Exercise/arrays_exercise1.php
@@ -4,8 +4,17 @@
 * Write a PHP script which will display the colors on the following way:
 * Output:
 * white, green, red,
+* green
+* red
+* white
 */
 $color = array('white', 'green', 'red');
+
+foreach ($color as $value) {
+	echo "$value, ";
+}
+sort($color);
+
 echo "<ul>";
 foreach ($color as $key=>$value) {
 	echo "<li>$value</li>";
